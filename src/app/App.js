290c5ac9d530001ef95330f2bd2de6769f630dbc
Rob FunGentoo <higheredbob@gmail.com>
2019-03-06 18:47:52 -0700

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import { connect } from "react-redux";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 import Post from './containers/Post'
 import Slug from './containers/Slug'
-
 import MainLayout from './layouts/MainLayout'
 
 const DashboardRoute = ({ component: Component, ...rest }) => {
@@ -18,13 +25,36 @@ const DashboardRoute = ({ component: Component, ...rest }) => {
   );
 };
 
-const Routes = () => (
-  <div>
-    <main>
-      <DashboardRoute exact path="/" component={Post} />
-      <DashboardRoute exact path="/:slug" component={Slug} />
-    </main>
-  </div>
-)
+class App extends Component {
 
-export default Routes
+  render() {
+    const { settings } = this.props;
+
+    return (
+      <div>
+        <MuiThemeProvider theme={settings.theme}>
+          <CssBaseline />
+            <Router>
+                  <Switch>
+                    <DashboardRoute exact path="/" component={Post} />
+                    <DashboardRoute exact path="/:slug" component={Slug} />
+                  </Switch>
+            </Router>
+        </MuiThemeProvider>
+      </div>
+        )
+    }
+}
+
+App.propTypes = {};
+
+const mapStateToProps = state => {
+  return {
+    settings: state.settings,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(App);
