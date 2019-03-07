2ab0f9cfddf6ddce4309ea1cd0830089595dbb28
Rob FunGentoo <higheredbob@gmail.com>
2019-03-06 18:22:26 -0700

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -3,12 +3,26 @@ import { Route, Link } from 'react-router-dom'
 import Post from './containers/Post'
 import Slug from './containers/Slug'
 
+import MainLayout from './layouts/MainLayout'
+
+const DashboardRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={matchProps => (
+        <MainLayout>
+          <Component {...matchProps} />
+        </MainLayout>
+      )}
+    />
+  );
+};
 
 const Routes = () => (
   <div>
     <main>
-      <Route exact path="/" component={Post} />
-      <Route exact path="/:slug" component={Slug} />
+      <DashboardRoute exact path="/" component={Post} />
+      <DashboardRoute exact path="/:slug" component={Slug} />
     </main>
   </div>
 )
